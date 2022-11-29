7bf33e92b1aae8bcb10daceb9184c7adb705217b
Aryan Singh <singh.aryan837@gmail.com>
2022-11-30 01:14:24 +0530

    spin-the-wheel

--- src/App.js
+++ src/App.js
@@ -1,25 +1,49 @@
-import logo from './logo.svg';
 import './App.css';
+import {React,useState} from 'react';
+import Wheel from './components/Wheel';
 
 function App() {
+  const [spinning,setSpinning]=useState(false);
+  const [winners,setWinners]=useState([]);
+  const [openModal,setOpenModal]=useState(false);
+  const [duration,setDuration]=useState(10);
+  const [fontColor,setFontColor]=useState('#FFFFFF');
+  const [wheelColor,setWheelColor]=useState('#d38c12');
+
+  const [items, setItems] = useState(() => {
+    const value = window.localStorage.getItem("itemsList");
+    return value !== null
+      ? JSON.parse(value)
+      : [
+          "Ali",
+          "Beatriz",
+          "Charles",
+          "Diya",
+          "Eric",
+          "Fatima",
+          "Gabriel",
+          "Hanna",
+        ];
+  });
+
+  function cancelModal() {
+    setOpenModal(false);
+  }
+
+
+  function removeWinnerModal() {
+    const winner = winners[winners.length - 1];
+    const index = items.indexOf(winner);
+    items.splice(index, 1);
+    setItems(items);
+    setOpenModal(false);
+    console.log(`Removed ${winner} from entries.`);
+    localStorage.setItem("itemsList", JSON.stringify(items));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <></>
+  )
 }
 
 export default App;
